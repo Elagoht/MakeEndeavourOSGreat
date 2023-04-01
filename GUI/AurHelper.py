1520eea43b7c2f95aa383d94123b5dc6174cc80b
Elagoht <furkanbaytekin@gmail.com>
2023-04-01 13:44:36 +0300

    refactor: AurHelper and Pamac Tabs

--- GUI/AurHelper.py
+++ GUI/AurHelper.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QWidget, QGroupBox, QPushButton, QLabel, QGridLayout
 from PyQt5.QtGui import QIcon
 from Result import CommandButton
 from os import popen
-from Utilities import run_command
+from Utilities import run_command, GridBox, ButtonBox
 
 
 class AurHelperTab(QWidget):
@@ -10,30 +10,35 @@ class AurHelperTab(QWidget):
         super(QWidget, self).__init__()
 
         # Create description section
-        self.gbxAur = QGroupBox("What is AUR Helper")
-        self.glyAur = QGridLayout(self.gbxAur)
+        self.gbxAur = GridBox("What is AUR Helper")
         self.lblAurDesc = QLabel("Arch User Repository packages needs to be install manually by cloning git repositories (technically downloading files from internet but in a cooler way), and running some commands that creating packages from recipe file. That's usually a few step process. In this point, AUR helpers helps you to install, update and uninstall packages from AUR and official repositories.")
         self.lblAurDesc.setWordWrap(True)
-        self.btnAurCheck = QPushButton(
+        self.btnAurCheck = CommandButton(
             QIcon("GUI/Assets/check.png"), "Check AUR Helpers", self.gbxAur)
         self.lblAurHelper = QLabel(self.gbxAur)
-        self.glyAur.addWidget(self.lblAurDesc)
-        self.glyAur.addWidget(self.btnAurCheck)
-        self.glyAur.addWidget(self.lblAurHelper)
+        self.gbxAur.addWidget(self.lblAurDesc)
+        self.gbxAur.addWidget(self.btnAurCheck, 1, 0)
+        self.gbxAur.addWidget(self.lblAurHelper, 2, 0)
 
         # Create paru section
-        self.gbxParu = QGroupBox("Paru", self)
-        self.glyParu = QGridLayout(self.gbxParu)
-        self.lblParu = QLabel(
-            "Paru is most popular AUR helper written in rust.")
-        self.lblParu.setWordWrap(True)
-        self.btnParuInstall = CommandButton(
-            QIcon("GUI/Assets/install.png"), "Install", self.gbxParu)
-        self.btnParuUninstall = CommandButton(
-            QIcon("GUI/Assets/uninstall.png"), "Uninstall", self.gbxParu)
-        self.glyParu.addWidget(self.lblParu, 0, 0, 1, 2)
-        self.glyParu.addWidget(self.btnParuInstall, 1, 0)
-        self.glyParu.addWidget(self.btnParuUninstall, 1, 1)
+        self.appParu = ButtonBox("Paru", "", "Paru is most popular AUR helper written in rust.", (
+            CommandButton(QIcon("GUI/Assets/install.png"),
+                          "Install", self),
+            CommandButton(QIcon("GUI/Assets/uninstall.png"),
+                          "Uninstall", self)
+        ), (
+            """if [ ! -f /bin/paru ]
+                then workdir=$(mktemp -d) &&
+                cd $workdir &&
+                git clone https://aur.archlinux.org/paru-bin.git &&
+                cd paru-bin &&
+                makepkg -si &&
+                rm -rf $workdir
+            fi""",
+            """if [ -f /bin/paru ]
+                then sudo pacman -R paru-bin || sudo pacman -R paru
+            fi"""
+        ))
 
         # Create yay section
         self.gbxYay = QGroupBox("Yay", self)
@@ -52,19 +57,6 @@ class AurHelperTab(QWidget):
         # Connect buttons to functions
         self.btnAurCheck.clicked.connect(
             lambda: self.lblAurHelper.setText(self.get_aur_helpers()))
-        self.btnParuInstall.clicked.connect(lambda: run_command(
-            """if [ ! -f /bin/paru ]
-    then workdir=$(mktemp -d) &&
-    cd $workdir &&
-    git clone https://aur.archlinux.org/paru-bin.git &&
-    cd paru-bin &&
-    makepkg -si &&
-    rm -rf $workdir
-fi""", self.btnParuInstall))  # Installing AUR helper without an AUR helper.
-        self.btnParuUninstall.clicked.connect(lambda: run_command(
-            """if [ -f /bin/paru ]
-    then sudo pacman -R paru-bin || sudo pacman -R paru
-fi""", self.btnParuUninstall))
         self.btnYayInstall.clicked.connect(lambda: run_command(
             """if [ ! -f /bin/yay ]
     then workdir=$(mktemp -d) &&
@@ -82,7 +74,7 @@ fi""", self.btnYayUninstall))
         # Insert groupboxes to layout
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self.gbxAur)
-        self.layout.addWidget(self.gbxParu)
+        self.layout.addWidget(self.appParu)
         self.layout.addWidget(self.gbxYay)
 
         # Initialize
--- GUI/Pamac.py
+++ GUI/Pamac.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QWidget, QGroupBox, QLabel, QVBoxLayout
 from PyQt5.QtGui import QIcon
 from Result import CommandButton
-from Utilities import aur_helper, has_aur_helper, run_command
+from Utilities import run_command, GridBox, AppBox
 
 
 class PamacTab(QWidget):
@@ -9,44 +9,21 @@ class PamacTab(QWidget):
         super(QWidget, self).__init__()
 
         # Create install section
-        self.gbxPamac = QGroupBox("Install Pamac", self)
-        self.glyPamac = QVBoxLayout(self.gbxPamac)
-        self.lblPamac = QLabel(
-            "Pamac is a graphical software manager/application market created by Manjaro team. It makes installing, updating and removing apps easy.",
-            self.gbxPamac)
-        self.lblPamac.setWordWrap(True)
-        self.btnInstall = CommandButton(
-            QIcon("GUI/Assets/install.png"), "Install", self.gbxPamac)
-        self.btnUninstall = CommandButton(
-            QIcon("GUI/Assets/uninstall.png"), "Uninstall", self.gbxPamac)
-        self.glyPamac.addWidget(self.lblPamac)
-        self.glyPamac.addWidget(self.btnInstall)
-        self.glyPamac.addWidget(self.btnUninstall)
-
+        self.appPamac = AppBox("Install Pamac", "pamac-aur", "GUI/Assets/Apps/pamac.png",
+                               "Pamac is a graphical software manager/application market created by Manjaro team. It makes installing, updating and removing apps easy.")
         # Create AUR section
-        self.gbxAUR = QGroupBox("AUR Support for Pamac", self)
-        self.glyAUR = QVBoxLayout(self.gbxAUR)
+        self.gbxAUR = GridBox("AUR Support for Pamac")
         self.lblAUR = QLabel("Arch User Repository is an additional package source. It's necessery for lots of package and application (like Spotify, Discord etc.). But be careful when installing unknown packages. Noone can guarantee that all of AUR packages is up to date and safe. You can use it but do not install packages you don't know or trust!")
         self.lblAUR.setWordWrap(True)
         self.btnEnable = CommandButton(
             QIcon("GUI/Assets/enabled.png"), "Enable AUR", self.gbxAUR)
         self.btnDisable = CommandButton(
             QIcon("GUI/Assets/disabled.png"), "Disable AUR", self.gbxAUR)
-        self.glyAUR.addWidget(self.lblAUR)
-        self.glyAUR.addWidget(self.btnEnable)
-        self.glyAUR.addWidget(self.btnDisable)
+        self.gbxAUR.addWidget(self.lblAUR, 0, 0, 1, 2)
+        self.gbxAUR.addWidget(self.btnEnable, 1, 0)
+        self.gbxAUR.addWidget(self.btnDisable, 1, 1)
 
         # Connect buttons to functions
-        self.btnInstall.clicked.connect(lambda: run_command(
-            f"""if [ ! "$(pacman -Qq pamac-aur)" = "pamac-aur" ]
-    then {aur_helper()} -S pamac-aur
-fi""" if has_aur_helper() else "false",
-            self.btnInstall))
-        self.btnUninstall.clicked.connect(lambda: run_command(
-            f"""if [ "$(pacman -Qq pamac-aur)" = "pamac-aur" ]
-    then {aur_helper()} -R pamac-aur
-fi""" if has_aur_helper() else "false",
-            self.btnUninstall))
         self.btnEnable.clicked.connect(lambda: run_command(
             "sudo sed -Ei '/EnableAUR/s/^#//' /etc/pamac.conf",
             self.btnEnable))
@@ -56,5 +33,5 @@ fi""" if has_aur_helper() else "false",
 
         # Insert groupboxes to layout
         self.layout = QVBoxLayout(self)
-        self.layout.addWidget(self.gbxPamac)
+        self.layout.addWidget(self.appPamac)
         self.layout.addWidget(self.gbxAUR)
