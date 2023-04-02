e5cd0ca179522f3af09e244dda42f9eb85ca6c26
Elagoht <furkanbaytekin@gmail.com>
2023-04-02 17:07:54 +0300

    feat: icon themes

--- GUI/Theming.py
+++ GUI/Theming.py
@@ -8,25 +8,35 @@ class ThemingTab(QWidget):
     def __init__(self):
         super(QWidget, self).__init__()
 
-        # Create fonts section
+        # Create themes section
         self.gbxTheme = GridBox("GTK Themes")
         self.lblTheme = QLabel(
-            "This is the primary theme for your interface and apps that use GTK framwork.", self.gbxTheme)
+            """<p>This is the primary theme for your interface and apps that use GTK framwork.</p>
+            <p>You MUST use "Awd-gtk3" to unify the themes. There are other themes. GTK 4 apps does not properly adapt to other themes yet.</p>""", self.gbxTheme)
         self.lblTheme.setWordWrap(True)
         self.gbxTheme.addWidget(self.lblTheme, 0, 0, 1, 3)
+        self.load_themes()
+
+        # Create icons section
+        self.gbxIcons = GridBox("Icons Themes")
+        self.lblIcons = QLabel(
+            "Icons themes determines how your apps, mimetypes, folders, actions, categories, emblems, panel status will look.", self.gbxIcons)
+        self.lblIcons.setWordWrap(True)
+        self.gbxIcons.addWidget(self.lblIcons, 0, 0, 1, 3)
+        self.load_icons()
 
         # Create fonts section
         self.gbxFont = GridBox("Nerd Fonts")
         self.lblFont = QLabel("Nerd fonts are font collections formed by combining alphanumerical and symbolic characters. Includes lots of font-icon and have wide use area. To be able to see font-icons instead of empty rectangle, install and use one of the following.", self.gbxFont)
         self.lblFont.setWordWrap(True)
         self.gbxFont.addWidget(self.lblFont, 0, 0, 1, 3)
+        self.load_fonts()
 
         # Insert groupboxes to layout
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self.gbxTheme)
+        self.layout.addWidget(self.gbxIcons)
         self.layout.addWidget(self.gbxFont)
-        self.load_fonts()
-        self.load_themes()
 
     def load_themes(self):
         with open("GUI/Data/Themes.json", "r") as themes_json:
@@ -41,6 +51,21 @@ class ThemingTab(QWidget):
                     self.gbxTheme.addWidget(ThemeBox(*theme.values()), 1, 2)
                 case _:
                     self.gbxTheme.glyField.addWidget(ThemeBox(*theme.values()))
+
+    def load_icons(self):
+        with open("GUI/Data/Icons.json", "r") as icons_json:
+            icons: list = load(icons_json)
+        for number, icon in enumerate(icons):
+            match number:
+                case 0:
+                    self.gbxIcons.addWidget(ThemeBox(*icon.values(), 1), 1, 0)
+                case 1:
+                    self.gbxIcons.addWidget(ThemeBox(*icon.values(), 1), 1, 1)
+                case 2:
+                    self.gbxIcons.addWidget(ThemeBox(*icon.values(), 1), 1, 2)
+                case _:
+                    self.gbxIcons.glyField.addWidget(
+                        ThemeBox(*icon.values(), 1))
 
     def load_fonts(self):
         with open("GUI/Data/Fonts.json", "r") as fonts_json:
