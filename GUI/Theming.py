35deca25afe1e0b6cf54aebfa93098d46e7b20de
Elagoht <furkanbaytekin@gmail.com>
2023-04-03 01:37:59 +0300

    feat: cursors and cursor image maker

--- GUI/Theming.py
+++ GUI/Theming.py
@@ -15,7 +15,7 @@ class ThemingTab(QWidget):
             <p>You MUST use "Awd-gtk3" to unify the themes. There are other themes. GTK 4 apps does not properly adapt to other themes yet.</p>""", self.gbxTheme)
         self.lblTheme.setWordWrap(True)
         self.gbxTheme.addWidget(self.lblTheme, 0, 0, 1, 3)
-        self.load_themes()
+        self.load_themes("GUI/Data/Themes.json", 0, self.gbxTheme)
 
         # Create icons section
         self.gbxIcons = GridBox("Icons Themes")
@@ -23,11 +23,20 @@ class ThemingTab(QWidget):
             "Icons themes determines how your apps, mimetypes, folders, actions, categories, emblems, panel status will look.", self.gbxIcons)
         self.lblIcons.setWordWrap(True)
         self.gbxIcons.addWidget(self.lblIcons, 0, 0, 1, 3)
-        self.load_icons()
+        self.load_themes("GUI/Data/Icons.json", 1, self.gbxIcons)
+
+        # Create cursor section
+        self.gbxCursor = GridBox("Cursor Themes")
+        self.lblCursor = QLabel(
+            "Cursor themes determines how your mouse pointer will look.", self.gbxCursor)
+        self.lblCursor.setWordWrap(True)
+        self.gbxCursor.addWidget(self.lblCursor, 0, 0, 1, 3)
+        self.load_themes("GUI/Data/Cursors.json", 2, self.gbxCursor)
 
         # Create fonts section
         self.gbxFont = GridBox("Nerd Fonts")
-        self.lblFont = QLabel("Nerd fonts are font collections formed by combining alphanumerical and symbolic characters. Includes lots of font-icon and have wide use area. To be able to see font-icons instead of empty rectangle, install and use one of the following.", self.gbxFont)
+        self.lblFont = QLabel(
+            "Nerd fonts are font collections formed by combining alphanumerical and symbolic characters. Includes lots of font-icon and have wide use area. To be able to see font-icons instead of empty rectangle, install and use one of the following.", self.gbxFont)
         self.lblFont.setWordWrap(True)
         self.gbxFont.addWidget(self.lblFont, 0, 0, 1, 3)
         self.load_fonts()
@@ -36,36 +45,23 @@ class ThemingTab(QWidget):
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self.gbxTheme)
         self.layout.addWidget(self.gbxIcons)
+        self.layout.addWidget(self.gbxCursor)
         self.layout.addWidget(self.gbxFont)
 
-    def load_themes(self):
-        with open("GUI/Data/Themes.json", "r") as themes_json:
+    def load_themes(self, file: str, type: int, widget: GridBox):
+        with open(file, "r") as themes_json:
             themes: list = load(themes_json)
         for number, theme in enumerate(themes):
             match number:
                 case 0:
-                    self.gbxTheme.addWidget(ThemeBox(*theme.values()), 1, 0)
+                    widget.addWidget(ThemeBox(*theme.values(), type), 1, 0)
                 case 1:
-                    self.gbxTheme.addWidget(ThemeBox(*theme.values()), 1, 1)
+                    widget.addWidget(ThemeBox(*theme.values(), type), 1, 1)
                 case 2:
-                    self.gbxTheme.addWidget(ThemeBox(*theme.values()), 1, 2)
+                    widget.addWidget(ThemeBox(*theme.values(), type), 1, 2)
                 case _:
-                    self.gbxTheme.glyField.addWidget(ThemeBox(*theme.values()))
-
-    def load_icons(self):
-        with open("GUI/Data/Icons.json", "r") as icons_json:
-            icons: list = load(icons_json)
-        for number, icon in enumerate(icons):
-            match number:
-                case 0:
-                    self.gbxIcons.addWidget(ThemeBox(*icon.values(), 1), 1, 0)
-                case 1:
-                    self.gbxIcons.addWidget(ThemeBox(*icon.values(), 1), 1, 1)
-                case 2:
-                    self.gbxIcons.addWidget(ThemeBox(*icon.values(), 1), 1, 2)
-                case _:
-                    self.gbxIcons.glyField.addWidget(
-                        ThemeBox(*icon.values(), 1))
+                    widget.glyField.addWidget(
+                        ThemeBox(*theme.values(), type))
 
     def load_fonts(self):
         with open("GUI/Data/Fonts.json", "r") as fonts_json:
