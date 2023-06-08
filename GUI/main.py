e52ab3e684b59a9839e31661bdfa3a89c975d034
Elagoht <furkanbaytekin@gmail.com>
2023-06-09 01:17:27 +0300

    chore: app icon added

--- GUI/main.py
+++ GUI/main.py
@@ -1,5 +1,6 @@
 #!/bin/env python3
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 from sys import argv, exit
 from MainWin import MainWin
 
@@ -8,5 +9,6 @@ app.setApplicationName("eos-tweaker")
 app.setApplicationDisplayName("Endeavour OS Tweaker")
 app.setApplicationVersion("v0.2")
 app.setStyle("kvantum")
+app.setWindowIcon(QIcon("Assets/icon.png"))
 main = MainWin()
 exit(app.exec_())
